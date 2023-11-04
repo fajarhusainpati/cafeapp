64eced12e7b006628674a83301ff4bc0fd8aa61b
bitinfinitywebsolutions <bitinfinitywebsolutionspvtltd@gmail.com>
2023-11-04 17:39:09 +0530

    Update otpverify.dart

--- lib/otpverify.dart
+++ lib/otpverify.dart
@@ -12,6 +12,61 @@ class OtpVerify extends StatefulWidget {
 class _OtpVerifyState extends State<OtpVerify> {
   @override
   Widget build(BuildContext context) {
+    // OTP TextField
+    Widget _buildOtpTextField() {
+      return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(right: 20.0),
+        width: 55,
+        height: 55,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: const Color.fromRGBO(235, 237, 237, 1),
+        ),
+        child: const TextField(
+          keyboardType: TextInputType.number,
+          maxLength: 1,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Color.fromRGBO(0, 10, 28, 1),
+            fontWeight: FontWeight.w700,
+            height: 1.40,
+          ),
+          decoration: InputDecoration(
+            counter: Offstage(),
+            border: InputBorder.none,
+          ),
+        ),
+      );
+    }
+
+// Submit Button
+    Widget _buildSubmitButton() {
+      return SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: MaterialButton(
+          elevation: 0.0,
+          highlightElevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 18.0),
+          color: kButtonColor,
+          onPressed: () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ResetPassword())),
+          child: const Text(
+            "Submit",
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
@@ -46,10 +101,10 @@ class _OtpVerifyState extends State<OtpVerify> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
+                    const Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text(
+                        Text(
                           "Enter OTP",
                           style: TextStyle(
                             color: kTextHeadingColor,
@@ -57,79 +112,33 @@ class _OtpVerifyState extends State<OtpVerify> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        const SizedBox(height: 20.0),
+                        SizedBox(height: 20.0),
                         Text(
                           "An 4 digit code has been sent to\n+91 8209444644",
                           style: TextStyle(
                             fontSize: 16.5,
-                            color: Colors.grey.shade600,
+                            color: Color.fromRGBO(117, 117, 117, 1),
                             fontWeight: FontWeight.w500,
                             height: 1.40,
                           ),
                         ),
-                        const SizedBox(height: 40.0),
+                        SizedBox(height: 40.0),
                       ],
                     ),
                     Container(
                       margin: const EdgeInsets.only(right: 8.0),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [1, 2, 3, 4]
-                            .map(
-                              (e) => Container(
-                                alignment: Alignment.center,
-                                margin: const EdgeInsets.only(right: 20.0),
-                                width: 55,
-                                height: 55,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: const Color.fromRGBO(235, 237, 237, 1),
-                                ),
-                                child: const TextField(
-                                  keyboardType: TextInputType.number,
-                                  maxLength: 1,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 20.0,
-                                    color: Color.fromRGBO(0, 10, 28, 1),
-                                    fontWeight: FontWeight.w700,
-                                    height: 1.40,
-                                  ),
-                                  decoration: InputDecoration(
-                                    counter: Offstage(),
-                                    border: InputBorder.none,
-                                  ),
-                                ),
-                              ),
-                            )
-                            .toList(),
+                        children: [
+                          _buildOtpTextField(),
+                          _buildOtpTextField(),
+                          _buildOtpTextField(),
+                          _buildOtpTextField(),
+                        ],
                       ),
                     ),
                     const SizedBox(height: 50.0),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: MaterialButton(
-                        elevation: 0.0,
-                        highlightElevation: 0.0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 18.0),
-                        color: kButtonColor,
-                        onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ResetPassword())),
-                        child: const Text(
-                          "Submit",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                    ),
+                    _buildSubmitButton()
                   ],
                 ),
               )
