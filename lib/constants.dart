020e0363df9973f96dfd8e45981b28ab3a9a97b6
chetannager <chetannager64@gmail.com>
2023-11-10 21:57:46 +0530

    colors updated!

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+const kPrimaryColor = Color.fromRGBO(59, 107, 175, 1);
 const kTextColor = Color.fromRGBO(144, 149, 154, 1);
 const kTextHeadingColor = Color.fromRGBO(25, 43, 77, 1);
 const kInputColor = Color.fromRGBO(171, 176, 182, 1);
 const kButtonColor = Color.fromRGBO(1, 100, 255, 1);
 const kBackButtonColor = Color.fromRGBO(98, 108, 130, 1);
 const kLinkColor = Color.fromRGBO(59, 107, 175, 1);
+const dividerColor = Color.fromRGBO(171, 176, 182, 1);
